048e5f3efcde19e0b20e6122d3a0fc8b971a0291
Kinasu <ashtoreet@gmail.com>
2017-11-26 17:55:01 +0300

    add authenticate to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,14 @@ class User < ApplicationRecord
 	def self.hash_to_string(password_hash)
 		password_hash.unpack('H*')[0]
 	end
+
+	def self.authenticate(name, password)
+		user = find_by(name: name)
+
+		if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+			user
+		else
+			nil
+		end
+	end
 end
