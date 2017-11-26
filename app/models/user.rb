559ca61efd308aff086a7a94d02fe186c302d7e1
Kinasu <ashtoreet@gmail.com>
2017-11-26 17:35:29 +0300

    user create

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+require 'openssl'
+
+class User < ApplicationRecord
+	ITERATIONS = 20000
+	DIGEST = OpenSSL::Digest::SHA256.new
+
+	attr_accessor :password
+
+	validates_presence_of :password, on: :create
+	validates_confirmation_of :password
+
+	before_save :encrypt_password
+
+	def encrypt_password
+		if self.password.present?
+			self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+			self.password_hash = User.hash_to_string(
+				OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+				)
+		end
+	end
+
+	def self.hash_to_string(password_hash)
+		password_hash.unpack('H*')[0]
+	end
+end
